e9d428f7f977d7c467bec7d327f6c91f335e5416
Lukas Filip <lukas@filipovi.eu>
2025-06-20 14:33:14 +0200

    Final result

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,7 +8,10 @@
 */
 
 
-using System; 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
 
 public class Program
 {
@@ -18,6 +21,10 @@ public class Program
 
         Console.WriteLine("Welcome to ToDoList!");
 
+
+        
+        
+        List<string> todoListItems = new List<string>();
 
         while (true) {
 
@@ -35,25 +42,106 @@ public class Program
             int optionId;
             bool option = int.TryParse(userInput, out optionId);
 
+            if (!option)
+            {
+                Console.WriteLine("Input was not an integer!");
+                continue;
+
+            }
+          
+
 
             if (option == false)
             {
-                Console.WriteLine("Please type correct number. The range is (0-3).");
+                Console.WriteLine("Please type correct number. The range is (0-3). \n");
 
                 continue; 
             }
 
             
+            if (optionId == 0)
+            {
+                Console.WriteLine("Program was successfully ended. \n" );
+                break;
+
+            }
+
+            if (optionId == 1)
+            {
+                foreach (string item in todoListItems) // write to console every part from ToDoList Items 
+                {
+                Console.WriteLine(item);
+                }
+
+
+
+                /* 
+                   for (int i = 0; i < todoListItems.Count; i++)
+                {
+                    string item = todoListItems[i];
+                    Console.WriteLine(item);
+
+                }
+                 
+                */
+
             
+            }
+
+            if (optionId == 2)
+            {
+
+                string newToDoList = Console.ReadLine(); // wait for user input 
+                
+               if (newToDoList != "")
+                {
+
+                    todoListItems.Add(newToDoList);
+                    Console.WriteLine("New item was successfully added. \n");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Item was not added. \n");
+                     
+                }
+                    
             
+
+            }
+
+            if (optionId == 3) // removing items 
+            {
+                Console.WriteLine("Please type a item what you want to remove:");
+
+                string userInputForRemoveIndex = Console.ReadLine();
+                int removeIndex;
+                bool removeIndexParseResult = int.TryParse(userInputForRemoveIndex, out removeIndex);
+                
+                if (removeIndexParseResult == false)
+                {
+                    Console.WriteLine("Input was not an integer!");
+                    continue; 
+
+                }
+
+                if (removeIndex < 0 || removeIndex >= todoListItems.Count)
+                {
+                    Console.WriteLine("Input must be non-negative and less than the size of the collection!");
+                    continue; 
+
+                }
+
+                todoListItems.RemoveAt(removeIndex);
+
+            }
             
+
+
             
-            
-            
-            //break; 
-        
-        
-        
+
+
         }
 
 
