b0fcc3cb06e7a8907738d56eb78d2bb3d67d09a0
Lukas Filip <lukas@filipovi.eu>
2025-06-20 13:01:14 +0200

    first try

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,5 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+          this is a simple ToDoList 
+             - allow us to exit 
+             - allow us to add items to our ToDoList
+             - allow us to remove items from our ToDoList 
+
+*/
 
 
-// just test ok? firt push to github
+using System; 
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        Console.WriteLine("Welcome to ToDoList!");
+
+
+        while (true) {
+
+
+            Console.WriteLine("Here are the options you can select:");
+
+            Console.WriteLine("0 - exit:");
+            Console.WriteLine("1 - view ToDo List");
+            Console.WriteLine("2 - add to ToDo list");
+            Console.WriteLine("3 - remove from ToDo list");
+
+
+
+            string userInput = Console.ReadLine(); // wait for user input
+            int optionId;
+            bool option = int.TryParse(userInput, out optionId);
+
+
+            if (option == false)
+            {
+                Console.WriteLine("Please type correct number. The range is (0-3).");
+
+                continue; 
+            }
+
+            
+            
+            
+            
+            
+            
+            
+            //break; 
+        
+        
+        
+        }
+
+
+    }
+
+
+
+
+
+}
